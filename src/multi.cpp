375b004474cdd1267fdb618816c1328b610fd31b
Jeeyong Um <conr2d@gmail.com>
2021-06-01 15:43:53 +0000

    Remove manual action dispatcher

--- src/multi.cpp
+++ src/multi.cpp
@@ -1,16 +1,2 @@
 #include "animal.hpp"
 #include "fruit.hpp"
-
-extern "C" {
-
-[[eosio::wasm_entry]]
-void apply(uint64_t receiver, uint64_t code, uint64_t action) {
-   if (code == receiver) {
-      switch (action) {
-         EOSIO_DISPATCH_HELPER(animal_contract, (buyanimal)(sellanimal))
-         EOSIO_DISPATCH_HELPER(fruit_contract, (buyfruit)(sellfruit))
-      }
-   }
-}
-
-}
